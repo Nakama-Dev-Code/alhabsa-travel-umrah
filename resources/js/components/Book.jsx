b662a7a4267a2f2ff009930f3333be154b519c49
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-04-03 17:02:03 +0700

    [UPDATE] Meningkatkan tampilan lebih baik pada tombol untuk book yaitu tombol cari keberangkatan

--- resources/js/components/Book.jsx
+++ resources/js/components/Book.jsx
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react';
 
+import {
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalTrigger,
+  } from "./ui/animated-modal";
+
 const Book = () => {
   // State untuk jenis perjalanan
   const [travelType, setTravelType] = useState('umroh-reguler');
@@ -314,12 +322,38 @@ Wassalamu'alaikum warahmatullahi wabarakatuh.
             placeholder="Masukkan kode promo"
             className="flex-grow p-2 border rounded w-full md:w-auto"
           />
-          <button
+          {/* <button
             onClick={sendToWhatsApp}
             className="bg-[#222636] text-white px-6 py-2 rounded hover:bg-[#2E3650] transition-colors  w-full md:w-auto mt-4 md:mt-0"
           >
             Cari Keberangkatan
-          </button>
+          </button> */}
+          <Modal>
+            <ModalTrigger className=" cursor-pointer bg-black dark:bg-white dark:text-black text-white flex justify-center group/modal-btn">
+                <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500">
+                Cari Keberangkatan
+                </span>
+                <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
+                ✈️
+                </div>
+            </ModalTrigger>
+
+            <ModalBody>
+
+                <ModalContent>
+                <p>Apakah Anda yakin ingin mengirim permintaan ini?</p>
+                </ModalContent>
+                <ModalFooter>
+                <button
+                    onClick={sendToWhatsApp}
+                    className="bg-green-500 text-white px-4 py-2 rounded"
+                >
+                    Kirim ke WhatsApp
+                </button>
+                </ModalFooter>
+            </ModalBody>
+            </Modal>
+
         </div>
       </div>
     </div>
